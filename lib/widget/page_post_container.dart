a24bad9cc2ba7152980a582b0b0d9bb44af23d10
pierre edimo <pedimonkoe@yahoo.com>
2022-04-03 23:18:15 +0200

    add textError

--- lib/widget/page_post_container.dart
+++ lib/widget/page_post_container.dart
@@ -3,14 +3,20 @@ import 'package:houlala/widget/list_of_posts.dart';
 
 class PagePostContainer extends StatelessWidget {
   final String? url;
+  final String? textError;
 
-  const PagePostContainer({Key? key, this.url}) : super(key: key);
+  const PagePostContainer({
+    Key? key,
+    this.url,
+    this.textError,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListOfPosts(
+      textError: textError,
       scrollDirection: Axis.vertical,
-      height: 0.25,
+      height: MediaQuery.of(context).size.height * 0.25,
       uri: url!,
     );
   }
