e1b3cc5a5c7dc4590f29074dcfe67ce4ff0fd073
pierre edimo <pedimonkoe@yahoo.com>
2022-04-03 23:18:41 +0200

    add height

--- lib/widget/post_container.dart
+++ lib/widget/post_container.dart
@@ -15,73 +15,75 @@ class PostContainer extends StatelessWidget {
           .pushNamed(PostDetailScreen.routeName, arguments: post!.id!),
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.9,
-        child: Card(
-          color: const Color(0xfefefefe),
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(post!.page!.imageUrl!),
-                ),
-                const SizedBox(
-                  width: 10.0,
-                ),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        post!.page!.name!,
-                        style: const TextStyle(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Card(
+            color: const Color(0xfefefefe),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(post!.page!.imageUrl!),
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          post!.page!.name!,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              fontFamily: "PoppinsBold"),
+                        ),
+                        Text(
+                          post!.title!,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
                             fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
-                            fontFamily: "PoppinsBold"),
-                      ),
-                      Text(
-                        post!.title!,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
+                            fontSize: 16.0,
+                          ),
                         ),
-                      ),
-                      Text(
-                        post!.content!,
-                        maxLines: post!.imageUrl!.isEmpty ? 5 : 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 14
+                        Text(
+                          post!.content!,
+                          maxLines: post!.imageUrl!.isEmpty ? 5 : 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 14
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 5.0,
-                      ),
-                      post!.imageUrl!.isEmpty
-                          ? Container()
-                          : Container(
-                             constraints: const BoxConstraints(
-                               minHeight: 200
-                             ),
-                              width: MediaQuery.of(context).size.width * 1,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15.0),
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                    post!.imageUrl!,
+                        const SizedBox(
+                          height: 5.0,
+                        ),
+                        post!.imageUrl!.isEmpty
+                            ? Container()
+                            : Expanded(
+                              child: Container(
+                                  width: MediaQuery.of(context).size.width * 1,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        post!.imageUrl!,
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                  fit: BoxFit.cover,
                                 ),
-                              ),
                             ),
 
-                    ],
-                  ),
-                )
-              ],
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
