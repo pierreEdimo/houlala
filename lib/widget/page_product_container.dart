43999c12fd0c62f663d99f1fd30dd97db404a56f
pierre edimo <pedimonkoe@yahoo.com>
2022-04-03 23:18:28 +0200

    addd textError

--- lib/widget/page_product_container.dart
+++ lib/widget/page_product_container.dart
@@ -3,16 +3,24 @@ import 'package:houlala/widget/grid_of_products.dart';
 
 class PageProductContainer extends StatelessWidget {
   final String? uri;
+  final String? textError;
 
-  const PageProductContainer({Key? key, this.uri}) : super(key: key);
+  const PageProductContainer({
+    Key? key,
+    this.uri,
+    this.textError,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridOfProducts(
+      height: MediaQuery.of(context).size.height * 0.25,
+      textError: textError,
       scrollDirection: Axis.vertical,
       uri: uri!,
       heightRatio: 1.35,
       widthRatio: 1,
+      crossAxisCount: 2,
     );
   }
 }
