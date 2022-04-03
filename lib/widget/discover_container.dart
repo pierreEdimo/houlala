07836f10c4e69a54e33b5eac37a534d25380d99b
pierre edimo <pedimonkoe@yahoo.com>
2022-04-03 22:16:38 +0200

    add jobs in the discover widget

--- lib/widget/discover_container.dart
+++ lib/widget/discover_container.dart
@@ -3,7 +3,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:houlala/widget/column_of_shop_box.dart';
 import 'package:houlala/widget/custom_intern_navigation.dart';
 import 'package:houlala/widget/discover_container_box.dart';
+import 'package:houlala/widget/grid_of_jobs.dart';
 import 'package:houlala/widget/list_of_posts.dart';
+import 'package:houlala/widget/standard_custom_container.dart';
 
 class DiscoverContainer extends StatelessWidget {
   DiscoverContainer({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class DiscoverContainer extends StatelessWidget {
     "produits",
     "magasins",
     "posts",
+    "jobs",
   ];
 
   final List<Widget> _widgetOptions = [
@@ -35,10 +38,21 @@ class DiscoverContainer extends StatelessWidget {
     Padding(
       padding: const EdgeInsets.all(10.0),
       child: ListOfPosts(
+        textError: "Aucuns Posts",
         scrollDirection: Axis.vertical,
         uri: dotenv.env['POST_URL'],
       ),
     ),
+   StandardCustomContainer(
+      child: GridOfJobs(
+        uri: dotenv.env['JOB_URL'],
+        scrollDirection: Axis.vertical,
+        ratio: 2.5,
+        textError: "Desole Nous n'avons pas encore de Jobs pour l'instant\n"
+            "reessayez plus tard",
+        height: 0.7,
+      ),
+    )
   ];
 
   @override
