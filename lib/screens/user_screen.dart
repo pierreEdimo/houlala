e0a73e8bec060de1d905a24a27b00735cc047f68
pierre edimo <pedimonkoe@yahoo.com>
2022-04-15 10:24:04 +0200

    edit the userscreen

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:houlala/service/auth_service.dart';
+import 'package:houlala/widget/sign_in_container.dart';
 import 'package:houlala/widget/standard_app_bar.dart';
+import 'package:provider/provider.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    final String? logInState =
+        Provider.of<AuthService>(context).getLoggedState();
+
+    return Scaffold(
       appBar: StandardAppBar(
-        title: "Mon Compte",
+        title: "houlala",
       ),
-      body: const  Center(
-        child: Text("Hello User"),
-      ),
+      body: logInState != 'loggedIn'
+          ? const SignInContainer()
+          : Center(
+              child: ElevatedButton(
+                  onPressed: () =>
+                      Provider.of<AuthService>(context, listen: false).logOut(),
+                  child: const Text("Log Out")),
+            ),
     );
   }
 }
