241eb491e19a5e79ad936276ee94f346df6e3695
pierre edimo <pedimonkoe@yahoo.com>
2022-04-17 18:11:57 +0200

    creer les inputs standards pour les numeros

---
+++ lib/widget/input_number.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class InputNumber extends StatelessWidget {
+  final TextEditingController? controller;
+  final String? errorMessage;
+  final String? hintText;
+
+  const InputNumber({
+    Key? key,
+    this.controller,
+    this.errorMessage,
+    this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(hintText: hintText),
+      validator: (value) {
+        value = controller!.text;
+        if (value.isEmpty) {
+          return errorMessage;
+        }
+        return null;
+      },
+    );
+  }
+}
