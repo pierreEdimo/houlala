f790329e221810132662106990d34f4e3691a956
pierre edimo <pedimonkoe@yahoo.com>
2022-04-17 18:20:32 +0200

    implementer les methodes d'authentification

--- lib/widget/sign_in_container.dart
+++ lib/widget/sign_in_container.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:houlala/model/login.dart';
 import 'package:houlala/service/auth_service.dart';
-import 'package:houlala/widget/email_input.dart';
-import 'package:houlala/widget/password_input.dart';
+import 'package:houlala/widget/input_email.dart';
+import 'package:houlala/widget/input_password.dart';
 import 'package:provider/provider.dart';
 import '../helper/constants.dart';
 import 'custom_elevated_button.dart';
@@ -23,82 +24,83 @@ class _SignInContainerState extends State<SignInContainer> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15.0),
       child: SingleChildScrollView(
-        child: SizedBox(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(
-                  height: 50.0,
-                ),
-                const Text(
-                  "Bienvenu sur Houlala",
-                  style: TextStyle(fontSize: 24.0),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                EmailInput(
-                  controller: _emailController,
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                PasswordInput(
-                  controller: _passwordController!,
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                CustomElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      print(_passwordController!.text);
-                      print(_emailController!.text);
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(
+                height: 50.0,
+              ),
+              const Text(
+                "Bienvenu sur Houlala",
+                style: TextStyle(fontSize: 24.0),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              EmailInput(
+                controller: _emailController,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              PasswordInput(
+                controller: _passwordController!,
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              CustomElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
 
-                    }
+                     Login login = Login(
+                       email: _emailController!.text,
+                       password: _passwordController!.text
+                     );
 
-                    Provider.of<AuthService>(context, listen: false).logIn();
+                    Provider.of<AuthService>(context, listen: false).logIn(login);
 
-                  },
-                  child: Text(
-                    "Se Connecter",
-                    style: standardStyle,
+                  }
+
+                },
+                child: Text(
+                  "Se Connecter",
+                  style: standardStyle,
+                ),
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              Row(
+                children: [
+                  const Text(
+                    "Nouveau sur houlala?",
                   ),
-                ),
-                const SizedBox(
-                  height: 25.0,
-                ),
-                Row(
-                  children: [
-                    const Text(
-                      "Nouveau sur houlala?",
-                    ),
-                    const SizedBox(
-                      width: 5.0,
-                    ),
-                    GestureDetector(
-                      onTap: () => print("hello World!"),
-                      child: const Text(
-                        "S'enregistrer",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Text(
-                    "Mot de passe oublier? / Problèmes de connexion",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                  const SizedBox(
+                    width: 5.0,
                   ),
-                )
-              ],
-            ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pushNamed("/signUp"),
+                    child: const Text(
+                      "S'enregistrer",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: const Text(
+                  "Mot de passe oublier? / Problèmes de connexion",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
           ),
         ),
       ),
