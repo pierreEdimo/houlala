cedc22ef81c5333ffe9c701e7ef55da023367610
pierre edimo <pedimonkoe@yahoo.com>
2022-04-20 01:21:04 +0200

    ajouter le displaydialog

---
+++ lib/widget/display_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+Future<void> showErrorDialog(
+    context,
+    String title,
+    String msg,
+    ) async {
+  return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text(
+                  msg,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 14.0 ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text("ok"),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      });
+}
