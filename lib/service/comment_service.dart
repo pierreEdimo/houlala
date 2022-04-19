a0fedf16f744af4f1a484d72171a8eec75a7fe6f
pierre edimo <pedimonkoe@yahoo.com>
2022-04-20 01:20:05 +0200

    ajouter la classe CommentService

---
+++ lib/service/comment_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:houlala/model/add_comment.dart';
+import 'package:houlala/model/comment.dart';
+import 'package:http/http.dart';
+
+class CommentService extends ChangeNotifier {
+  Future<List<Comment>> fetchAllCategories(String uri) async {
+    var url = Uri.parse(uri);
+    Response response = await get(url);
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+
+      List<Comment> comments =
+          body.map((dynamic comment) => Comment.fromJson(comment)).toList();
+      return comments;
+    } else {
+      throw "No Categories";
+    }
+  }
+
+  Future<void> addComment(AddComment comment) async {
+    Map<String, String> headers = {'Content-Type': 'application/json'};
+    String jsEncode;
+    jsEncode = jsonEncode(comment);
+    var url =
+        Uri.parse('${dotenv.env['POST_URL']}/addComment/${comment.postId}');
+    Response response =await patch(
+      url,
+      headers: headers,
+      body: jsEncode,
+    );
+    notifyListeners();
+  }
+}
