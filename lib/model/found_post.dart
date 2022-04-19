bebc668174dceabe4e51cd4af8a3496818ca1109
pierre edimo <pedimonkoe@yahoo.com>
2022-04-20 01:21:47 +0200

    ajouter la variable isLiked.

--- lib/model/found_post.dart
+++ lib/model/found_post.dart
@@ -5,15 +5,18 @@ class FoundPost {
     this.foundPost,
     this.commentCount,
     this.likesCount,
+    this.isLiked,
   });
 
   final Post? foundPost;
   final int? commentCount;
   final int? likesCount;
+  bool? isLiked;
 
   factory FoundPost.fromJson(Map<String, dynamic> json) => FoundPost(
         foundPost: Post.fromJson(json["foundPost"]),
         commentCount: json["commentCount"] as int,
         likesCount: json["likesCount"] as int,
+        isLiked: json['isLiked'] as bool,
       );
 }
