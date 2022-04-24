60a26f3d1ab28b24281666444646b28a77e6440c
pierre edimo <pedimonkoe@yahoo.com>
2022-04-24 14:15:25 +0200

    Objet cartError,

---
+++ lib/widget/cart_error.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:houlala/widget/standard_custom_container.dart';
+
+class CartError extends StatelessWidget {
+  const CartError({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: StandardCustomContainer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            ImageIcon(
+              AssetImage('images/cart.png'),
+              size: 140,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text("Il n'y'a aucuns Articles dans le Panier.",
+            textAlign: TextAlign.center,)
+          ],
+        ),
+      ),
+    );
+  }
+}
