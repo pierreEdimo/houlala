8b0a430c7dfd2e0fba393d9d2787a0bb6fb3e615
pierre edimo <pedimonkoe@yahoo.com>
2022-04-24 14:19:10 +0200

    classe CountAndPrice

---
+++ lib/model/CountAndPrice.dart
@@ -0,0 +1,16 @@
+
+class CountAndPrice {
+  final int? totalPrice;
+  final int? totalQuantity;
+
+  CountAndPrice({
+    this.totalPrice,
+    this.totalQuantity
+  });
+
+  factory CountAndPrice.fromJson(Map<String, dynamic> json) =>
+      CountAndPrice(
+        totalPrice: json['totalPrice'] as int,
+        totalQuantity: json['totalQuantity'] as int,
+      );
+}
