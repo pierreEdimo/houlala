c7d582f2be9e58e59eb19d52d51a53e6c8fae715
pierre edimo <pedimonkoe@yahoo.com>
2022-04-24 14:17:36 +0200

    classe CartItemScreen

---
+++ lib/screens/cart_item_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:houlala/widget/list_of_cart_items.dart';
+import 'package:houlala/widget/standard_app_bar.dart';
+
+class CartItemScreen extends StatelessWidget {
+  const CartItemScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: StandardAppBar(
+        title: "Mon Panier",
+      ),
+      body: const ListOfCartItems(),
+    );
+  }
+}
