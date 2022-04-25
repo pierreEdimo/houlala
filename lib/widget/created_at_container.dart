04b842808edb673e742d66a007c66e01515ffc1c
pierre edimo <pedimonkoe@yahoo.com>
2022-04-25 10:41:37 +0200

    ajouter un container pour le Datetime.

---
+++ lib/widget/created_at_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CreatedAtContainer extends StatelessWidget {
+  final DateTime? createdAt;
+
+  const CreatedAtContainer({
+    Key? key,
+    this.createdAt,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      DateFormat.yMMMMEEEEd('fr').format(
+        createdAt!,
+      ),
+      style: const TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 12.0),
+    );
+  }
+}
