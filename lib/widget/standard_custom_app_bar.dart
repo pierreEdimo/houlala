2a3de8ab28bab23e8a78b30cd4127e8f4ff0d74b
pierre edimo <pedimonkoe@yahoo.com>
2022-05-10 00:59:51 +0200

    ajouter le Container pour le standardCustomappbar

--- lib/widget/standard_custom_app_bar.dart
+++ lib/widget/standard_custom_app_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'custom_button_container.dart';
 
-class CustomAppBar extends StatelessWidget with PreferredSizeWidget{
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   final Size preferredSize;
 
@@ -16,26 +16,33 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget{
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
+    return Material(
       elevation: 1,
-      centerTitle: false,
-      title: Text(
-        title!,
-        textAlign: TextAlign.left,
-      ),
-      actions: [
-        CustomButtonContainer(
-          icon: const FaIcon(FontAwesomeIcons.magnifyingGlass),
-          onPressed: () => Navigator.of(context).pushNamed("/search"),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        color: const Color(0xf2f2f2f2),
+        child: SafeArea(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title!,
+                  style: const TextStyle(
+                    fontFamily: 'PoppinsBold',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22.0
+                  ),
+                ),
+                CustomButtonContainer(
+                  icon: const FaIcon(FontAwesomeIcons.magnifyingGlass),
+                  onPressed: () => Navigator.of(context).pushNamed("/search"),
+                )
+              ],
+            ),
+          ),
         ),
-        // CustomButtonContainer(
-        //   icon: const FaIcon(FontAwesomeIcons.newspaper),
-        //   onPressed: () => print("news"),
-        // ),
-      ],
-      automaticallyImplyLeading: false,
+      ),
     );
   }
-
-
 }
