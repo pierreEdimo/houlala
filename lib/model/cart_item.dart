6240a750887c91f093e4d6207f66da69dca114e8
pierre edimo <pedimonkoe@yahoo.com>
2022-08-07 15:31:05 +0200

    modifier le CartItem

--- lib/model/cart_item.dart
+++ lib/model/cart_item.dart
@@ -1,38 +1,23 @@
-import 'package:houlala/model/product.dart';
-import 'package:hive/hive.dart';
-
-part 'cart_item.g.dart';
-
-@HiveType(typeId: 1)
-class CartItem extends HiveObject{
-  @HiveField(0)
-  final String? id;
-
-  @HiveField(1)
-  final String? userId;
-
-  @HiveField(2)
-  int? quantity;
-
-  @HiveField(3)
-  int? totalPrice;
-
-  @HiveField(4)
-  final Product? product;
+class CartItem {
+  final String? productSku;
+  final int? price;
+  final String? name;
+  final String? imageUrl;
+  final int? quantity;
 
   CartItem({
-    this.id,
-    this.userId,
+    this.productSku,
+    this.price,
+    this.name,
+    this.imageUrl,
     this.quantity,
-    this.totalPrice,
-    this.product,
   });
 
   factory CartItem.fromJson(Map<String, dynamic> json) => CartItem(
-        id: json['_id'] as String,
-        userId: json['userId'] as String,
-        quantity: json['quantity'] as int,
-        totalPrice: json['totalPrice'] as int,
-        product: Product.fromJson(json['product']),
+        productSku: json['productSku'],
+        price: json['price'],
+        name: json['product'],
+        imageUrl: json['imageUrl'],
+        quantity: json['quantity'],
       );
 }
