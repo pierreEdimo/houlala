62030ac906600e88e6398b3494ebcb301e622850
pierre edimo <pedimonkoe@yahoo.com>
2022-08-07 15:29:51 +0200

    modifier l'Address object

--- lib/model/address.dart
+++ lib/model/address.dart
@@ -1,40 +1,35 @@
-
-import 'package:hive/hive.dart';
-
-part 'address.g.dart';
-
-@HiveType(typeId: 5)
 class Address {
   Address({
     this.city,
     this.country,
     this.streetName,
     this.poBox,
+    this.houseNumber,
   });
 
-  @HiveField(0)
   final String? city;
 
-  @HiveField(1)
   final String? country;
 
-  @HiveField(2)
   final String? streetName;
 
-  @HiveField(3)
   final String? poBox;
 
+  final String? houseNumber;
+
   factory Address.fromJson(Map<String, dynamic> json) => Address(
-    city: json["city"],
-    country: json["country"],
-    streetName: json["streetName"],
-    poBox: json["poBox"],
-  );
+        city: json["city"],
+        country: json["country"],
+        streetName: json["streetName"],
+        poBox: json["poBox"],
+        houseNumber: json["houseNumber"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "city": city,
-    "country": country,
-    "streetName": streetName,
-    "poBox": poBox,
-  };
+        "city": city,
+        "country": country,
+        "streetName": streetName,
+        "poBox": poBox,
+        "houseNumber": houseNumber,
+      };
 }
