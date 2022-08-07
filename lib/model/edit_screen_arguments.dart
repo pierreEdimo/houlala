8aee1d687006c963cfee66eab0def23e6a88e03f
pierre edimo <pedimonkoe@yahoo.com>
2022-08-07 15:38:46 +0200

    Creer l'editScreenArguments

---
+++ lib/model/edit_screen_arguments.dart
@@ -0,0 +1,25 @@
+class EditScreenArguments {
+  final String? type;
+  final String? name;
+  final String? city;
+  final String? country;
+  final String? email;
+  final String? telephoneNumber;
+  final String? streetName;
+  final String? poBox;
+  final String? houseNumber;
+  final String? userName;
+
+  EditScreenArguments({
+    this.type,
+    this.name,
+    this.email,
+    this.country,
+    this.city,
+    this.telephoneNumber,
+    this.streetName,
+    this.houseNumber,
+    this.userName,
+    this.poBox,
+  });
+}
