492ff51e9379a7f79a21b74a0854b9805da9502e
pierre edimo <pedimonkoe@yahoo.com>
2022-08-07 15:36:50 +0200

    refactoring de l'Author

--- lib/model/comment.dart
+++ lib/model/comment.dart
@@ -4,22 +4,16 @@ class Comment {
   final String? content;
   final DateTime? createdAt;
   final String? id;
-  final Author? author;
   final String? postId;
+  final Author? author;
 
-  Comment({
-    this.postId,
-    this.content,
-    this.createdAt,
-    this.id,
-    this.author
-  });
+  Comment({this.postId, this.content, this.createdAt, this.id, this.author});
 
   factory Comment.fromJson(Map<String, dynamic> json) => Comment(
-    content: json['content'] as String,
-    id: json['_id'] as String,
-    author: Author.fromJson(json['user']),
-    postId: json['postId'] as String,
-    createdAt: DateTime.parse(json['createdAt']),
-  );
+        content: json['content'] as String,
+        id: json['_id'] as String,
+        postId: json['postId'] as String,
+        createdAt: DateTime.parse(json['createdAt']),
+        author: Author.fromJson(json['author']),
+      );
 }
